27e925571ee5db7316c3960247ebc1187435de21
John Duncan <john@johnduncanphoto.com>
2017-12-06 18:01:09 +0000

    can concat two arrays test passed

---
+++ array_tasks.js
@@ -0,0 +1,38 @@
+var arrayTasks = {
+
+	concat: function (arr1, arr2) {
+		var result = arr1.concat(arr2);
+		return result;
+	},
+
+	// insertAt: function (arr, itemToAdd, index) {
+
+	// },
+
+	// square: function (arr) {
+
+	// },
+
+	// sum: function (arr) {
+
+	// },
+
+	// findDuplicates: function (arr) {
+
+	// },
+
+	// removeAndClone: function (arr, valueToRemove) {
+
+	// },
+
+	// findIndexesOf: function (arr, itemToFind) {
+
+	// },
+
+	// sumOfAllEvenNumbersSquared: function (arr) {
+
+	// }
+
+}
+
+module.exports = arrayTasks
